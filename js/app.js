6c1d6fa9aac9ecd8771fdff0e74f501ea73d2ef9
duffield44 <duffieldbenjamin44@gmail.com>
2015-02-27 17:55:28 +0800

    Store data from ajax call in localStorage. Call upon localStorage when Make is selected

--- js/app.js
+++ js/app.js
@@ -5,21 +5,34 @@ $(function(){
 			api_key: 'fk5fszh84rrtvy5kz3jj9pey'
 		};
 
-	var cars = $.ajax({
+	$.ajax({
 			url: "https://api.edmunds.com/api/vehicle/v2/makes",
 			data: request,
 			dataType: "json",
 			type: "GET",
 		})
-		.done(function(result){
-			console.log(result);
+		.done(function(result){			
 
-			$.each(result.makes, function(i, item){
-				$('#make').append('<option value="0">' + item.name + '</option>');	
-			});
+			$.each(result.makes, function(i, make){
+				$('#make').append('<option value="' + i + '">' + make.name + '</option>');	
+			});		
+
+			var carData = JSON.stringify(result);
+			window.localStorage.setItem('cars', carData);
+		});	
+
+	
+	// Select a Make and the corresponding Models will be selected from localStorage
+	$('#make').change(function(){
+
+		// Removes previous car models if a make had been previously selected 
+		$('#model option').slice(1).remove();
+		
+		var makeSelection = $('#make option:selected').val();			
+		var carData = JSON.parse(window.localStorage.getItem('cars'));
+		$.each(carData.makes[makeSelection].models, function(i, model){
+			$('#model').append('<option value="' + i + '">' + model.name + '</option>');
 		});
-
-	
-	
+	});
 });
 
